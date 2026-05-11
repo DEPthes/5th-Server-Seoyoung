6196f9ab08910077a03201c1001b98086c714c6e
ksy132336 <ksygiru@gmail.com>
2026-05-12 00:19:46 +0900

    Docker Swarm Todo 애플리케이션 실습 추가

---
+++ week2/tododb/prepare.sh
@@ -0,0 +1,47 @@
+#!/bin/bash -e
+
+# (1) MasterとSlaveを環境変数で制御する
+if [ ! -z "$MYSQL_MASTER" ]; then
+  echo "this container is master"
+  exit 0
+fi
+
+echo "prepare as slave"
+
+# (2) SlaveからMasterへの疎通確認をする
+if [ -z "$MYSQL_MASTER_HOST" ]; then
+  echo "mysql_master_host is not specified" 1>&2
+  exit 1
+fi
+
+while :
+do
+  if mysql -h $MYSQL_MASTER_HOST -u root -p$MYSQL_ROOT_PASSWORD -e "quit" > /dev/null 2>&1 ; then
+    echo "MySQL master is ready!"
+    break
+  else
+    echo "MySQL master is not ready"
+    sleep 3
+  fi
+done
+
+# (3) Masterにレプリケーション用のユーザーと権限の作成
+IP=$(cat /etc/hosts | awk 'END{print $1}')
+IFS='.'
+set -- $IP
+SOURCE_IP="$1.$2.%.%"
+
+mysql -h $MYSQL_MASTER_HOST -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS '$MYSQL_REPL_USER'@'$SOURCE_IP' IDENTIFIED BY '$MYSQL_REPL_PASSWORD';"
+mysql -h $MYSQL_MASTER_HOST -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT REPLICATION SLAVE ON *.* TO '$MYSQL_REPL_USER'@'$SOURCE_IP';"
+
+# (4) Masterのbinlogのポジションを取得
+MASTER_STATUS_FILE=/tmp/master-status
+mysql -h $MYSQL_MASTER_HOST -u root -p$MYSQL_ROOT_PASSWORD -e "SHOW MASTER STATUS\G" > $MASTER_STATUS_FILE
+
+BINLOG_FILE=$(cat $MASTER_STATUS_FILE | grep File | awk '{print $2}')
+BINLOG_POSITION=$(cat $MASTER_STATUS_FILE | grep Position | awk '{print $2}')
+
+# (5) Slaveのレプリケーション開始
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CHANGE MASTER TO MASTER_HOST='$MYSQL_MASTER_HOST', MASTER_USER='$MYSQL_REPL_USER', MASTER_PASSWORD='$MYSQL_REPL_PASSWORD', MASTER_LOG_FILE='$BINLOG_FILE', MASTER_LOG_POS=$BINLOG_POSITION;"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "START SLAVE;"
+
